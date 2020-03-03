638e828677c8df5ce4e9ccd1d578d86ca6cc3e98
willsn <willsn@gmail.com>
2020-03-03 10:16:54 -0800

    add text to homepage, change color

--- pages/index.py
+++ pages/index.py
@@ -16,17 +16,13 @@ column1 = dbc.Col(
         dcc.Markdown(
             """
         
-            ## Your Value Proposition
+            ## What kind of product will people buy?
 
-            Emphasize how the app will benefit users. Don't emphasize the underlying technology.
-
-            ✅ RUN is a running app that adapts to your fitness levels and designs personalized workouts to help you improve your running.
-
-            ❌ RUN is the only intelligent running app that uses sophisticated deep neural net machine learning to make your run smarter because we believe in ML driven workouts.
+            This web app makes it easy to predict the types of products that different customers will purchase.
 
             """
         ),
-        dcc.Link(dbc.Button('Your Call To Action', color='primary'), href='/predictions')
+        dcc.Link(dbc.Button('Try it!', color='primary'), href='/predictions')
     ],
     md=4,
 )
