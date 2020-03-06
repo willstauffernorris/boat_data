a1d3effdab030cb0e8b433751ce024ac7dc85658
willsn <willsn@gmail.com>
2020-03-05 16:58:00 -0800

    two new features added

--- pages/predictions.py
+++ pages/predictions.py
@@ -26,6 +26,31 @@ column1 = dbc.Col(
             marks={n: str(n) for n in range(1,365,50)}, 
             className='mb-5', 
         ), 
+
+         
+        dcc.Markdown('#### Page Depth'), 
+        dcc.Slider(
+            id='Page Depth', 
+            min=1, 
+            max=135, 
+            step=5, 
+            value=1, 
+            marks={n: str(n) for n in range(1,135,20)}, 
+            className='mb-5', 
+        ), 
+
+        
+        dcc.Markdown('#### Session Duration'), 
+        dcc.Markdown('##### In Seconds'), 
+        dcc.Slider(
+            id='Session Duration', 
+            min=1, 
+            max=8000, 
+            step=20, 
+            value=800, 
+            marks={n: str(n) for n in range(0,8000,1000)}, 
+            className='mb-5', 
+        ), 
         
     ],
     md=4,
@@ -39,20 +64,19 @@ column2 = dbc.Col(
 )
 
 
-import pandas as pd
 
 import pandas as pd
 
 @app.callback(
     Output('prediction-content', 'children'),
-    [Input('Day_of_year', 'value')],
+    [Input('Day_of_year', 'value'), Input('Page Depth', 'value'), Input('Session Duration', 'value')],
 )
-def predict(Day_of_year):
+def predict(Day_of_year, Page_Depth, Session_Duration):
     df = pd.DataFrame(
-        columns=['Day_of_year'], 
-        data=[[Day_of_year]]
+        columns=['Day_of_year', 'Page Depth', 'Session Duration'], 
+        data=[[Day_of_year, Page_Depth, Session_Duration]]
     )
     y_pred = pipeline.predict(df)[0]
-    return f'{y_pred:.0f} dollars'
+    return f'That transaction will be worth about ${y_pred:.0f}.'
 
 layout = dbc.Row([column1, column2])
